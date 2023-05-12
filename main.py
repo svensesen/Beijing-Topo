28307f8cf94ff14e123cfefac6110215fdf433f3
svensesen <sven@svensteens.nl>
2023-05-12 10:39:56 +0200

    Expanded tool functionality

--- main.py
+++ main.py
@@ -1,53 +1,83 @@
-from dash import Dash, html, dcc, Input, Output
+from dash import Dash, html, dcc, Input, Output, State
 import pandas as pd
 
-# Counter to be able to send signals
-dataset_filter_counter = 0
-
-import_df = pd.DataFrame() # Importing the dataframe TBI
-user_list = ["000", "001", "002"] # List of all users TBI (should get this from the df)
+user_list = ["0", "1", "2"] #list(set(import_df["user"])) # List of all users 
 transportation_list = ["Walk", "Bike", "Bus"] # List of all transportation"s TBI (should get this from the df)
 graphing_method_list = ["Angle"] # List of all graphing methods TBI
+presets_list = ["Empty", "Small"]
 
 # Creates the dash app
 app = Dash()
 
 app.layout = html.Div([
-    html.Div("User:", style={"display": "inline-block"}),
-    dcc.Dropdown(options=["All"] + user_list, value="All", id="user_filter_dropdown", style={"display": "inline-block", "width": "20%"}),
-    html.Div("Transportation:", style={"display": "inline-block"}),
-    dcc.Dropdown(options=["All"] + transportation_list, value="All", id="transportation_filter_dropdown", style={"display": "inline-block", "width": "20%"}),
-    html.Div("Graphing Method:", style={"display": "inline-block"}),
-    dcc.Dropdown(options=graphing_method_list, value=graphing_method_list[0], id="graphing_method_dropdown", style={"display": "inline-block", "width": "20%"}),
+    dcc.RadioItems(options=["Presets", "Custom"], value="Presets", id="data_type_radio",inline=True),
 
+    html.Div([html.Div("Preset:", style={"display": "inline-block"}),
+    dcc.Dropdown(options=presets_list, value=presets_list[0], id="preset_dropdown", 
+                 style={"display": "inline-block"})],
+                 id = "presets_div", style={"display": "inline-block", "width": "20%"}),
+
+    # Filter on user if filtering
+    html.Div([html.Div("User:", style={"display": "inline-block"}),
+    dcc.Dropdown(options=["All"] + user_list, value="All", id="user_filter_dropdown", 
+                 style={"display": "inline-block", "width": "45%"}),
+    
+    # Filter on transportation if filtering
+    html.Div("Transportation:", style={"display": "inline-block"}),
+    dcc.Dropdown(options=["All"] + transportation_list, value="All", id="transportation_filter_dropdown", 
+                 style={"display": "inline-block", "width": "45%"}),], 
+                 id = "filtering_div", style = {"display": "none", "width": "40%"}),
+    
+    # Choose the method for graphing
+    html.Div("Graphing Method:", style={"display": "inline-block"}),
+    dcc.Dropdown(options=graphing_method_list, value=graphing_method_list[0], id="graphing_method_dropdown", 
+                 style={"display": "inline-block", "width": "20%"}),
+    
+    # Runs the application
+    html.Button("Run", id="run_button", style={"display": "inline-block"}),
+
+    # Space for the two graphs
     dcc.Graph(id="base_visualization_graph", config={"displayModeBar": False}, style={"display": "inline-block", "width": "40%"}),
     dcc.Graph(id="graph_visualization_graph", config={"displayModeBar": False}, style={"display": "inline-block", "width": "40%"}),
-
-    dcc.Store(data=[0], id="dataset_filter"), # Keeps track of whenever the dataset is filtered
 ])
 
 @app.callback(
-    Output("dataset_filter", "data"),
-    Input("user_filter_dropdown", "value"),
-    Input("transportation_filter_dropdown", "value"),
+    Output("filtering_div", "style"),
+    Output("presets_div", "style"),
+    Input("data_type_radio", "value"),
 )
-def filter(user, transportation):
-    global dataset_filter_counter, df
-    dataset_filter_counter += 1
-
-    # Use user and transportation to filter TBI
-    df = import_df
-
-    return [dataset_filter_counter]
-
-
+def active_filter(data_type):
+    if data_type == "Presets":
+        return {"display": "none"}, {"display": "inline-block"}
+    elif data_type == "Custom":
+        return {"display": "inline-block"}, {"display": "none"}
+    
 @app.callback(
     Output("base_visualization_graph", "figure"),
     Output("graph_visualization_graph", "figure"),
-    Input("graphing_method_dropdown", "value"),
-    Input("dataset_filter", "data"),
+    Input("run_button", "n_clicks"),
+    State("data_type_radio", "value"),
+    State("preset_dropdown", "value"),
+    State("user_filter_dropdown", "value"),
+    State("transportation_filter_dropdown", "value"),
+    prevent_initial_call=True
 )
-def filter(graphing_method, dummy):
+def run(dummy, data_type, preset, user, transportation):
+    if data_type == "Presets":
+        if preset == "empty":
+            df = pd.DataFrame()
+        elif preset == "Small":
+            df = pd.read_csv("small")
+
+    elif data_type == "Custom":
+        df = pd.read_csv("full_data.csv")
+
+        if user != "All":
+            df = df[df["user"] == user]
+        
+        if transportation != "All":
+            df = df[df["transportation"] == transportation]
+
     # Create the base visualization using the filtered 'df' dataframe TBI
     base_visualization = None 
 
