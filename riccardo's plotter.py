6e6e28822ff1ecb510ef63af1c7b59dab783f988
riccardo-torlaini <riccardotorlainiofficial@gmail.com>
2023-05-31 19:30:46 +0200

    she logic on my top till i data

---
+++ riccardo's plotter.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import os 
+def plotter(data:dict, marker = 'o-'):
+  for key in data.keys():
+    for point in data[key]:
+      plt.scatter(point[0], point[1], marker = marker)
+
+def data_plot(folder:None):
+  import pandas as pd
+  import numpy as np
+  import os
+  
+  if folder:
+    path = os.listdir(folder)
+  else:
+    path = os.listdir()
+  pt_dict = {}
+  for fileplt in path:
+    if fileplt.endswith('.plt'):
+      data_i = np.genfromtxt(path + '/' + fileplt, delimiter=',', skip_header=6)
+      pt_dict[fileplt] = [list(data[:3]) for data in data_i]
+
+  plotter(pt_dict)
+
